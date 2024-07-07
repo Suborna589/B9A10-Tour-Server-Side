a4324d90e12c3ff34f5c952d36519e9013edf970
Suborna589 <chakrobortis101@gmail.com>
2024-07-07 19:27:50 +0600

    post,get,put.delete almost done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const express = require('express'); 
 const cors = require( 'cors'); 
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express() ;
 const port =process.env.PORT || 5000;
 
@@ -27,13 +27,77 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect();  
+
+ const spotCollection=client.db('spotDB').collection('spot'); 
+
+
+app.get('/spot',async(req,res)=>{
+  const cursor=spotCollection.find();
+  const result=await cursor.toArray(); 
+  res.send(result)
+}) 
+
+app.get('/spot/:id', async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)};
+  const result=await spotCollection.findOne(query);
+  res.send(result)
+})
+
+
+
+    app.post('/spot', async(req,res)=>{ 
+      const newSpot = req.body; 
+      console.log(newSpot); 
+      const result=await spotCollection.insertOne(newSpot);
+      res.send(result);
+
+  }) 
+  
+  app.put('/list/:id',async(req,res)=>{ 
+    const id=req.params.id;
+    const filter={_id: new ObjectId(id)}
+ const options={upsert:true}; 
+ const updateSpot=req.body;
+
+ const Spot={ 
+  $set:{ 
+    image:updateSpot.image,
+    spot:updateSpot.spot,
+    country:updateSpot.country,
+    location:updateSpot.location,
+    cost:updateSpot.cost,
+    seasonality:updateSpot.seasonality,
+    time:updateSpot.time,
+    visit:updateSpot.visit,
+    description:updateSpot.description,
+
+  }
+
+ }
+ const result=await spotCollection.updateOne(filter,Spot,options) 
+ res.send(result)
+  }
+
+  )
+
+
+  app.delete('/spot/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: new ObjectId (id)}; 
+    const result=await spotCollection.deleteOne(query);
+    res.send(result);
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+  
+    
   }
 }
 run().catch(console.dir);
@@ -48,5 +112,3 @@ app.listen(port,()=>{
     console.log(`Torism port:${port}`)
 }) 
 
-//HRzeq7W8QLqTqMzw 
-//tour-master
